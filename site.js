0cd4331792092639b0eeab0cdbc27ca8a1c8a57d
abilling <abilling@hawk.iit.edu>
2018-12-06 22:43:51 -0600

    Added more JS to the file, adjusting CSS and html files to accomodate, Menu now shows up on the home resume page.

--- site.js
+++ site.js
@@ -10,16 +10,23 @@ var html = document.querySelector('html');
 html.classList.remove('no-js');
 html.classList.add('js');
 
-var navigation = document.queryselector('#navigation');
-var nav_list = document.queryselector('.nav');
+var navigation = document.querySelector('#navigation');
+var nav_list = document.querySelector('.nav');
+
 var nav_heading = document.createElement('h2');
 var menu_link = document.createElement('a');
+menu_link.textContent = 'Menu';
+menu_link.setAttribute('id','menu-button');
+menu_link.setAttribute('href','#null');
+menu_link.setAttribute('tabindex','0');
 
-menu_link.textContext = 'Menu';
+
+
 nav_heading.append(menu_link);
-navigation.insertBefore(nav_heading,nav_list);
+menu_link
+navigation.insertBefore(nav_heading, nav_list);
 
 var menu_button = document.querySelector('#menu-button');
-menu-button.addEventListener('click', function() {
+menu_button.addEventListener('click', function() {
   nav_list.classList.toggle('visible');
 });
