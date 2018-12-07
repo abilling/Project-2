918d37883f6e2cd0ef7c5aa7e240abdd308c34ab
abilling <abilling@hawk.iit.edu>
2018-12-06 23:38:44 -0600

    Added JS linter, attempted to use, also ran all files through both linters and validators and everything came up clean once fixed. Pretty happy overall with this project.

--- site.js
+++ site.js
@@ -1,32 +1,20 @@
-/*
- * This is a JavaScript Scratchpad.
- *
- * Enter some JavaScript, then Right Click or choose from the Execute Menu:
- * 1. Run to evaluate the selected text (Ctrl+R),
- * 2. Inspect to bring up an Object Inspector on the result (Ctrl+I), or,
- * 3. Display to insert the result in a comment after the selection. (Ctrl+L)
- */
+"use strict";
 var html = document.querySelector('html');
-html.classList.remove('no-js');
-html.classList.add('js');
-
 var navigation = document.querySelector('#navigation');
 var nav_list = document.querySelector('.nav');
-
 var nav_heading = document.createElement('h2');
 var menu_link = document.createElement('a');
+html.classList.remove('no-js');
+html.classList.add('js');
 menu_link.textContent = 'Menu';
-menu_link.setAttribute('id','menu-button');
-menu_link.setAttribute('href','#null');
-menu_link.setAttribute('tabindex','0');
-
-
+menu_link.setAttribute('id', 'menu-button');
+menu_link.setAttribute('href', '#null');
+menu_link.setAttribute('tabindex', '0');
 
 nav_heading.append(menu_link);
-menu_link
 navigation.insertBefore(nav_heading, nav_list);
-
 var menu_button = document.querySelector('#menu-button');
-menu_button.addEventListener('click', function() {
+menu_button.addEventListener('click', function(e) {
   nav_list.classList.toggle('visible');
+  e.preventDefault();
 });
